d662475b7ff0a6735e299dbdb1817be218d80556
xiillii <jarommat@gmail.com>
2020-04-10 06:07:47 -0500

    Complete validation and Picker for rating in entry page

--- TripLog/Views/NewEntryPage.xaml.cs
+++ TripLog/Views/NewEntryPage.xaml.cs
@@ -1,4 +1,7 @@
 using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using TripLog.ViewModels;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -22,7 +25,45 @@ namespace TripLog.Views
 
         private void Page_BindingContextChanged(object sender, EventArgs e)
         {
+            ViewModel.ErrorsChanged += ViewModel_ErrorsChanged;
+        }
 
+        private void ViewModel_ErrorsChanged(object sender, DataErrorsChangedEventArgs e)
+        {
+            var propHasErrors =
+                (ViewModel.GetErrors(e.PropertyName) as List<string>)?.Any() == true;
+
+            Color color;
+
+            // Getting the dynamic color. Dark Theme or Light Theme
+            if (Application.Current.Resources != null)
+            {
+                var y = Application.Current.Resources.FirstOrDefault(x => x.Key == "TextColorLabel");
+                if (y.Key != null)
+                {
+                    color = (Color)y.Value;
+                }
+                else
+                {
+                    color = Color.Default;
+                }
+            }
+            else
+            {
+                color = Color.Default;
+            }
+
+            switch (e.PropertyName)
+            {
+                case nameof(ViewModel.Title):
+                    title.LabelColor = propHasErrors ? Color.Red : color;
+                    break;
+                case nameof(ViewModel.Rating):
+                    rating.TextColor = propHasErrors ? Color.Red : color;
+                    break;
+                default:
+                    break;
+            }
         }
     }
 }
